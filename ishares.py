df70b6622ecd7b1daf1e8db27982104a12eddeb8
Michael Sorg <sorg.mi@gmail.com>
2021-03-10 10:37:38 +0100

    download csv from ishares

--- ishares.py
+++ ishares.py
@@ -1,5 +1,12 @@
+import re
+
 import pandas as pd
 import collections
+
+import requests
+import wget
+import urllib.request
+from bs4 import BeautifulSoup
 
 
 def read_csv(path):
@@ -10,14 +17,34 @@ def read_csv(path):
     df = df[['ISIN', 'Name', 'Weight (%)', 'Location']]
     df.columns = ['ISIN', 'name', 'weight', 'location']
     df.attrs['path'] = path
-    # todo add name of etf
+    # TODO add name of etf
 
     return df
 
 
+def get_file(url):
+    req = requests.get(url)
+    soup = BeautifulSoup(req.content, 'html.parser')
+    suburl = soup.select('#holdings > div.holdings.fund-component-data-export > a.icon-xls-export')[0].get('href')
 
+    # set filename
+    search = 'fileName=[a-zA-Z]*'
+    x = re.findall(search, suburl)
+    filename = x[0].split("=")[1] + ".csv"
 
+    # Download file
+    #filepath = wget.download(url + suburl, out=filename)
+    with urllib.request.urlopen(url + suburl) as f:
+        html = f.read().decode('utf-8')
+        with open(filename, 'w', encoding="utf-8") as out:
+            out.write(html)
 
+    return filename
+
+def getFromURL(url):
+    file = get_file(url)
+    df = read_csv(file)
+    return df
 
 
 
@@ -27,5 +54,9 @@ def read_csv(path):
 
 if __name__ == '__main__':
     d = read_csv('ICLN_holdings.csv')
+    print(d.head())
+    print(d.shape)
+
+    d = getFromURL('https://www.ishares.com/us/products/239738/')
     print(d.head())
     print(d.shape)
